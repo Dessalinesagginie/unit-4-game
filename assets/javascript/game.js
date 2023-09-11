d0e77b703f7538c4f422653908d07ee8072f2869
Whatevaulik <Dessagginie@gmail.com>
2023-09-11 14:53:55 -0500

    2nd Draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,22 @@
-$("#crystals-collector").text(Math.floor(Math.random() * 100));
+$(".blue").on("click", function() {
+    alert("blue");
+});
+
+$(".green").on("click", function() {
+    alert("green");
+});
+
+$(".red").on("click", function() {
+    alert("red");
+});
+
+$(".yellow").on("click", function() {
+    alert("yellow");
+});
+
+//generate random number between
+
+
+//screen display of random number
+
+$("#crystal-collector").text(Math.floor(Math.random() * 50));
