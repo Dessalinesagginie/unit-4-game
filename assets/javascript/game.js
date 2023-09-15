c244743789261b095f4347c8b190eaf943c263fe
Whatevaulik <Dessagginie@gmail.com>
2023-09-15 17:06:33 -0500

    Draft 3.5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,24 @@
+
+// set target equal to random number
+var target = $("#crystal-collector").text(Math.floor(Math.random() * 120 ));
+
+
+
+var bluePoints
+var redPoints
+var greenPoints
+var yellowPoints
+var wins
+var losses
+var playerScore  
+
+//("#random-blue")+("#random-two")+("#random-three")+("#random-four")
+
 $(".blue").on("click", function() {
 
     //generate specific crystal amt
     $("#random-blue").text(Math.floor(Math.random() * 12));
-
+   
     //add specific crystal amount to player's  score
 });
 
@@ -18,10 +34,9 @@ $(".yellow").on("click", function() {
     $("#random-four").text(Math.floor(Math.random() * 12));
 });    
 
-$("#crystal-collector").text(Math.floor(Math.random() * 120 ));
+$("#crystal-collector").text(Math.floor(Math.random() * 120 ));{
 
 //     * Your game will hide this amount until the player clicks a crystal.
 //     * When they do click one, update the player's score counter
 
-
-
+}
