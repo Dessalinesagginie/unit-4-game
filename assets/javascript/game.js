7506bbbdb448d421f120410a34803ff111b3f4f5
Whatevaulik <Dessagginie@gmail.com>
2023-10-05 12:31:12 -0500

    Reset vs reload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,21 +7,28 @@ var wins;
 var losses;
 var playerScore; 
 
-function scoreCompare() {
-    if (target === playerScore) {
-        wins += 1
-        $("#wins").text(wins)
-    }   
-    else {
-        losses += 1
-        $("#losses").text(losses);
+function randomIntFromInterval(min, max) 
+{
+    return Math.floor(Math.random() * (max - min +1)+ min);
+}
+
+function scoreCompare() 
+{
+    if (target === playerScore)
+    { 
+        wins += 1;
+        $("#wins").text(wins);
+    }  
+    else 
+    {
+        if (target < playerScore)
+        {
+            losses += 1;
+            $("#losses").text(losses);
+        }
     }
 };
 
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min +1)+ min);
-}
- 
 target = randomIntFromInterval(19, 120);
 bluePoints = randomIntFromInterval(1, 12);
 redPoints = randomIntFromInterval(1, 12); 
@@ -39,32 +46,35 @@ $("#player-score").text(playerScore);
 
 $("#crystal-collector").text(target);
 
-$(".blue").on("click", function() {
+$(".blue").on("click", function() 
+{
     playerScore = playerScore + bluePoints; 
-    scoreCompare();
     $("#player-score").text(playerScore);
+    scoreCompare();
 });
 
-$(".green").on("click", function() {
+$(".green").on("click", function() 
+{
     playerScore = playerScore + greenPoints;
     $("#player-score").text(playerScore);
+    scoreCompare();
 });
 
-$(".red").on("click", function() {
+$(".red").on("click", function() 
+{
     playerScore = playerScore + redPoints;
     $("#player-score").text(playerScore);
+    scoreCompare();
 });
 
-$(".yellow").on("click", function() {
+$(".yellow").on("click", function() 
+{
     playerScore = playerScore + yellowPoints;
     $("#player-score").text(playerScore);
+    scoreCompare();
 });
 
-
-
-
-// store a win
- 
-
-// playerScore = playerScore + greenPoints;
-// record a loss
+// reset game
+    // playerScore = 0
+    // target is reset, new random number
+    // cpoints = reset to new random#
