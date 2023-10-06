d913b79c2421d9db2d01e18e2c1f0602254c7a96
Whatevaulik <Dessagginie@gmail.com>
2023-10-06 08:42:59 -0500

    It's Owne!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,18 +6,18 @@ var yellowPoints;
 var wins;
 var losses;
 var playerScore; 
-
 function randomIntFromInterval(min, max) 
 {
     return Math.floor(Math.random() * (max - min +1)+ min);
 }
-
 function scoreCompare() 
 {
     if (target === playerScore)
     { 
         wins += 1;
         $("#wins").text(wins);
+        gameStart();
+        targetNew() 
     }  
     else 
     {
@@ -25,10 +25,11 @@ function scoreCompare()
         {
             losses += 1;
             $("#losses").text(losses);
+            gameStart();
+            targetNew() 
         }
     }
 };
-
 target = randomIntFromInterval(19, 120);
 bluePoints = randomIntFromInterval(1, 12);
 redPoints = randomIntFromInterval(1, 12); 
@@ -37,36 +38,28 @@ yellowPoints = randomIntFromInterval(1, 12);
 wins = 0;
 losses = 0;
 playerScore = 0; 
-
 $("#wins").text(wins);
-
 $("#losses").text(losses);
-
 $("#player-score").text(playerScore); 
-
 $("#crystal-collector").text(target);
-
 $(".blue").on("click", function() 
 {
     playerScore = playerScore + bluePoints; 
     $("#player-score").text(playerScore);
     scoreCompare();
 });
-
 $(".green").on("click", function() 
 {
     playerScore = playerScore + greenPoints;
     $("#player-score").text(playerScore);
     scoreCompare();
 });
-
 $(".red").on("click", function() 
 {
     playerScore = playerScore + redPoints;
     $("#player-score").text(playerScore);
     scoreCompare();
 });
-
 $(".yellow").on("click", function() 
 {
     playerScore = playerScore + yellowPoints;
@@ -74,7 +67,16 @@ $(".yellow").on("click", function()
     scoreCompare();
 });
 
-// reset game
-    // playerScore = 0
-    // target is reset, new random number
-    // cpoints = reset to new random#
+function gameStart() 
+{
+    target = randomIntFromInterval(19, 120);
+    bluePoints = randomIntFromInterval(1, 12);
+    redPoints = randomIntFromInterval(1, 12); 
+    greenPoints = randomIntFromInterval(1, 12);
+    yellowPoints = randomIntFromInterval(1, 12);
+    playerScore = 0; 
+    $("#crystal-collector").text(target);
+    $("#player-score").text(playerScore); 
+}
+
+
