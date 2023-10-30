4d1fce44e61b138bead8399070cb6ebdb2c57024
Whatevaulik <Dessagginie@gmail.com>
2023-10-30 08:34:09 -0500

    Should'vebeensimple

--- assets/javascript/StarW.js
+++ assets/javascript/StarW.js
@@ -6,6 +6,15 @@
 $(".Ahsoka").on("click", function() {
   $("#title").text("Ahsoka");
 });
+$(".Kylo Ren").on("click", function() {
+  $("#title").text("Kylo Ren");
+});
+$(".Ahsoka").on("click", function() {
+  $("#title").text("Ahsoka");
+});
+$(".Ahsoka").on("click", function() {
+  $("#title").text("Ahsoka");
+});
 
 // The player will fight as that character for the rest of the game. -->
 
