2d8f098e7fb226e68e4856730837c5b8014a7e4b
Whatevaulik <Dessagginie@gmail.com>
2023-11-02 16:53:53 -0500

    11.2.23 for Kids

--- assets/javascript/StarW.js
+++ assets/javascript/StarW.js
@@ -1,16 +1,23 @@
 
 
 
-
+let Ashoka = 0
 
 // click to select Your character 
-function selectYourCharacter() {
-  console.log("hello");
+function selectYourCharacter(Ashoka, y) {
+  return Ashoka + y;
+  
 }
 
 $("#Ahsoka").on("click", function() {
-  selectYourCharacter();
+  selectYourCharacter(Ashoka, 100);
+  // set value of Ashoka = 1
+  Ashoka = Ashoka + 1;
+  $("#heading3").text(Ashoka);
 });
+
+
+
 $(".Kylo-Ren").on("click", function() {
   selectYourCharacter();
 });
